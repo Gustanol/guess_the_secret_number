efd041d53ea1dd67b39545074c367c27131b1201
Gustanol <gualeixos456@gmail.com>
2025-01-14 15:59:14 -0300

    adding README.md

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ import java.util.InputMismatchException;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int randomNumber = (int)(Math.random() * 101);
+    int randomNumber = (int)(Math.random() * 101); // atribui um número aleatório entre 0 e 100 à variável
    
     System.out.println("Adivinhe o número!\n");
     
     while (true) {
       try {
         System.out.println("Digite um número inteiro de 0 a 100: ");
-        int number = scanner.nextInt();
+        int number = scanner.nextInt(); // captura o número digitado enquanto o loop estiver em execução
         
         if (number == randomNumber) {
-          System.out.println("Parabéns! Você adivinhou o número!");
+          System.out.println("\nParabéns! Você adivinhou o número!");
           break;
         } else if (number > randomNumber) {
             System.out.println("\nO número é menor! Tente novamente!");
@@ -22,8 +22,9 @@ public class Main {
             System.out.println("\nO número é maior! Tente novamente!");
         }
       } catch (InputMismatchException e) {
+          // captura erros de tipos de variáveis
           System.out.println("\nPor favor! Digite um número válido!\n");
-          scanner.nextLine();
+          scanner.nextLine(); // "limpa" o scanner para evitar loops infinitos
       }
     }
     scanner.close();
---
+++ README.md
@@ -0,0 +1,17 @@
+# My first Java project ✨
+
+### It's simple. The program will 'generate' a random number between 0 (included) and 101 (not included):
+
+`int randomNumber = (int)(Math.random() * 101);` 🤓
+
+While you do not enter the right number, the program will be executed. 💻
+
+---
+
+## Clone this repository: 🤖
+
+`git clone https://github.com/Gustanol/guess_the_secret_number.git`
+
+## Execute the program: 😎
+
+`java Main.java`
